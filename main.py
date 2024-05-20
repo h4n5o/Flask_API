baca00bc4832c9e169264f9ad3c7df0ee7ba5f9c
h4n5o <72876852+h4n5o@users.noreply.github.com>
2024-05-20 14:05:43 +0200

    erfolgreiche Get Anfrage.

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Api, Resource
 
 app = Flask (__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
-    def get(self):
-        return{"data": "Hello World"}
+    def get(self, name, test):
+        return{"name": name, "test": test}
 
-    def post(self):
-        return{"data": "Posted"}
+    
 
-    def put(self):
-        data = request.get_json()
-        return {"data": data}, 201
-
-    def delete(self):
-        return {"data": "Deleted"}, 204
-
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
 
 if __name__ == "__main__":
     app.run(debug=True)
