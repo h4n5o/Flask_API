462f76b115df83a17a963749eb6aa347912ecdf7
h4n5o <72876852+h4n5o@users.noreply.github.com>
2024-05-20 14:20:33 +0200

    minor adjustments

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from flask_restful import Api, Resource
 app = Flask (__name__)
 api = Api(app)
 
+names = {
+    "igor": {"age": 19, "gender": "male"},
+    "bill": {"age": 70, "gender": "male"}
+    }
+
 class HelloWorld(Resource):
-    def get(self, name, test):
-        return{"name": name, "test": test}
+    def get(self, name):
+        return names[name]
 
     
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
